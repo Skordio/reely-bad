cf189bb5404c875f9d22241da72b317e149d1e12
Skordio <skordion@hotmail.com>
2024-05-04 12:05:13 -0400

    update the matrix puzzle

--- src/pages/the-matrix.vue
+++ src/pages/the-matrix.vue
@@ -9,11 +9,17 @@
             <v-col :cols="mdAndDown ? '12': '6'" class="text-subtitle-1 d-flex flex-column" style="gap:18px">
                 <div>
                     “This... all of this... it's a lot to take in. 
-                    You're telling me that everything, every moment up until now was just... artificial? Fabricated?” 
+                    You're telling me that everything, my whole life... artificial? Fabricated?” 
                 </div>
                 <div>
-                    “Yes, Neo,” I said, “you’ve lived your whole life in a place where the only reality you know is artificially crafted and force fed to you. 
-                    If you had lived your whole life only allowed to see shadows, you would believe that was all there was.” 
+                    “Yes, Neo,” I said, “you’ve lived in a place where the only reality you know is artificially crafted and force fed to you. 
+                    If you had lived your whole life only allowed to see shadows on the wall, you would believe that was all there was.” 
+                </div>
+                <div>
+                    “We have to tell someone! They deserve to know the true nature of the world they live in,” he said.
+                </div>
+                <div>
+                    "No, Neo," said I. "We never free anyone above a certain age, because the mind has trouble letting go. If you were to go back into the cave and try to enlighten anyone to the truth, they would undoubtedly resist or worse."
                 </div>
 
                 <!-- <div>
@@ -40,15 +46,17 @@
                 </div>
                 <div class="font-italic">
                     They visit the Oracle, a serene figure surrounded by an air of mystery. 
-                    However, her response leaves them just as puzzled as before:
+                    This was her response, upon seeing the code they had found:
                 </div>
             </v-col>
             <v-divider :vertical="!mdAndDown" :style="mdAndDown ? '' : 'min-height:60vh'" :class="!mdAndDown ? 'my-auto' : ''"></v-divider>
             <v-col :cols="mdAndDown ? '12': '6'" class="d-flex flex-column align-center justify-center text-center text-h5">
                 <div class="mb-6">
-                    Seek an ancient archive where heroes' wisdom is kept. 
-                    Only in the writings of one who realized the nature of the veil will your path be cleared. 
-                    There, amidst the dialogue of sages, lies the connection.
+                    Ah, these coordinates you've found - they're the right ones, but you ought to look at them with a different perspective.
+                    You're gonna have to find a certain archive belonging to a famous hero. 
+                    There, somewhere amidst the dialogues of sages, are where the numbers point. 
+                    What you're looking for comes from someone who made a similar realization to the one you did, Neo.
+                    Also remember - every letter counts.
                 </div>
                 <div v-for="number in numbers">{{ number }}</div>
             </v-col>
